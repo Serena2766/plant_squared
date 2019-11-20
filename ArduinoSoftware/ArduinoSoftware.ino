29dc7c3e9db9039c02260f3736bab4d00d253ac5
juanpablocontreras <55557151+juanpablocontreras@users.noreply.github.com>
2019-11-20 12:58:16 -0500

    added get and perform command logic

--- ArduinoSoftware/ArduinoSoftware.ino
+++ ArduinoSoftware/ArduinoSoftware.ino
@@ -12,7 +12,7 @@ DHT dht(DHTPIN, DHTTYPE);
 //Global variables
 int currentLightLevel;
 bool isPlantPresent = false;
-bool wasPlantPresent = true;
+bool wasPlantPresent = false;
 byte command = 0;
 String serialMessage; //defined as global variable so arduino to reduce allocated memory
 
@@ -21,7 +21,7 @@ void setup() {
   //begin serial communication
   Serial.begin(9600);
 
-  //set A0 to analogue input for moisture sensor output
+  //set A0 to analogue input for moisture sensor data
   pinMode(A0, INPUT); 
 
   //begin DHT readings
@@ -48,7 +48,50 @@ void loop() {
  }
 
  
-  
+  //get and perform command
+  if(Serial.available() > 0){
+    //command available
+
+    command = Serial.read(); //read command and remove byte from buffer. if there is no plant command will be discarded anyway
+
+    //perform command if the plant is present
+    if(isPlantPresent){
+
+      //decipher command
+      if(command == B01110000){
+        
+        //read and send data from all the sensors
+        Serial.println("all data");
+        
+      }else if(command >> 4 == B00000001){
+        
+        //water the plant command = 0001 0+amount(3bits)
+        byte amount = command - B00010000;
+         
+        Serial.print("water plant ");
+        Serial.println(amount);
+        
+      }else if(command >> 5 == B00000001){
+        
+        //set light level 
+        byte amount = command - B00100000;
+        
+        Serial.print("set light level ");
+        Serial.println(amount);
+      }else{
+        //command not found
+        Serial.print("{\"error\":command not found} ");
+      }
+
+      Serial.print("command: ");
+      Serial.println(command);
+      
+    }else{
+      //command is received from client Raspberry PI, but the plant is not in the system. 
+      //send error message
+      Serial.println("{\"error\":command was received but plant is not in the system}");
+    }
+  }
 
 }
 
@@ -75,5 +118,9 @@ void setLightLevel(int level){
 }
 
 bool isPlantPresentFunc(){
-  return false;
+  return true;
+}
+
+void waterPlant(){
+  
 }
